2bb7eba671af0c141d39ef0485205183d202ec9c
selector <grayrubix.parent.@gmail.com>
2021-02-12 11:00:21 +0330

    Bourc Api

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { View , Text} from "react-native";
+import { View, Text, StatusBar, Image } from "react-native";
+import { styles } from '../styles/GlobalStyle';
+import {useSelector} from 'react-redux'
 
 const Home = () => {
-    return ( 
-        <View>
-        <Text >
-            Home
-        </Text>
+    const bourse = useSelector((state) => state.bourseData[0]);
+
+
+    useEffect(() =>{
+        console.log(bourse)
+    },[])
+    return (
+        <View style={styles.body}>
+            <StatusBar backgroundColor="#FFA500" />
+
         </View>
 
-     );
+    );
 }
- 
+
 export default Home;
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,11 @@
+import { fetchbourse } from "../../api/bourse";
+
+export const getBourse = () => {
+    return async (dispatch) => {
+        const bourse = await fetchbourse();
+        await dispatch({ type: "BOURSDATA", payload: bourse });
+    };
+};
 
 export const GetUserName = (data) => {
     return async (dispatch) => {
@@ -12,11 +20,5 @@ export const GetPassword = (data) => {
 export const GetInfo = (data) => {
     return async (dispatch) => {
         await dispatch({ type: "GETUINFO", payload: data })
-    };
-};
-export const getCourses = () => {
-    return async (dispatch) => {
-        const courses = await fetchCourses();
-        await dispatch({ type: "INIT", payload: courses });
     };
 };
